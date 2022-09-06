524a537057e0fda4ec7d9719bde5636459e6a801
lsdsjy <lsdsjy@163.com>
2022-09-06 14:27:07 +0800

    chore: dts

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import * as monaco from 'monaco-editor';
+
+export declare function createEditor(container: HTMLElement, template: string, workers: {
+	ts: string | Worker;
+	editor: string | Worker;
+}): {
+	monacoEditorInstance: monaco.editor.IStandaloneCodeEditor;
+	dispose: () => void;
+	onChange: (fn: (content: Record<string, string>) => void) => void;
+	validate: () => Promise<monaco.languages.typescript.Diagnostic[] | undefined>;
+	value: {};
+};
