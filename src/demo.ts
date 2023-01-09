d60a5ddd73e10e13d3ce9189970cedd73a35e087
lsdsjy <lsdsjy@163.com>
2023-01-09 07:23:46 +0000

    refactor: use esm.sh

--- src/demo.ts
+++ src/demo.ts
@@ -1,8 +1,6 @@
 import { createEditor } from './editor'
-const tsWorkerUrl =
-  'https://gist.githubusercontent.com/lsdsjy/93be41d6d2dd0927a745895c375eb6f8/raw/f0c5e22b622726f6b14d80dd1f0f326b7a908e28/ts.worker.js'
-const editorWorkerUrl =
-  'https://gist.githubusercontent.com/lsdsjy/c8c49fb748b1532b1462487b55b4a244/raw/44c75a1d009e1a6f5d0473be111a832cb078a4bb/editor.worker.js'
+import editorWorkerFactory from 'https://esm.sh/monaco-editor/esm/vs/editor/editor.worker?worker'
+import tsWorkerFactory from 'https://esm.sh/monaco-editor@0.34.1/esm/vs/language/typescript/ts.worker?worker'
 
 const typeDef= 
     ['type Config = Array<{',
@@ -13,8 +11,8 @@ const typeDef=
     ].join('\n');
 
 const editor = createEditor(document.getElementById('container')!, typeDef,{
-  ts: tsWorkerUrl,
-  editor:editorWorkerUrl
+  ts: tsWorkerFactory(),
+  editor: editorWorkerFactory()
 });
 
 editor.onChange(async (content) => {
--- src/shim.d.ts
+++ src/shim.d.ts
@@ -2,6 +2,11 @@ declare module 'constrained-editor-plugin' {
   export const constrainedEditor: any
 }
 
+declare module 'https://esm.sh/*' {
+  const factory: () => Worker
+  export default factory
+}
+
 // FIXME: this won't work
 declare module 'monaco-editor/esm/vs/editor.api' {
   export namespace editor {
